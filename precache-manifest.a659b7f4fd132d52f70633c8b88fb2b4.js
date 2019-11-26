35cbfbcff4df41deed9d77c63951ae0a018c85ea
THEEP_PC <THEEP_PC@DESKTOP-HUK8OKM>
2019-11-26 23:16:34 +0200

    Updates

---
+++ precache-manifest.a659b7f4fd132d52f70633c8b88fb2b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c01bb787e8105147af13c9b1b8deb0",
+    "url": "/ultra/index.html"
+  },
+  {
+    "revision": "7dc4aaa17614edf5e7fd",
+    "url": "/ultra/static/css/main.f56b5951.chunk.css"
+  },
+  {
+    "revision": "d87559d936da3d252202",
+    "url": "/ultra/static/js/2.6b61d473.chunk.js"
+  },
+  {
+    "revision": "7dc4aaa17614edf5e7fd",
+    "url": "/ultra/static/js/main.a065c89d.chunk.js"
+  },
+  {
+    "revision": "78fdc546353bbd2fd362",
+    "url": "/ultra/static/js/runtime-main.ec30b0f4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github.io/ultra/precache-manifest.fe6aa9c3099c046b68958f08ee74ca17.js"
+  "/ultra/precache-manifest.a659b7f4fd132d52f70633c8b88fb2b4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.io/ultra/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ultra/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
